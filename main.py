b146b6c1bd5ae126ae9c51d221602f1af863ea57
yoshinoniwa <yoshinoniwa>
2018-12-07 02:31:27 +0900

    フォルダ内のファイルをグラフで表示

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt
+import glob
+ 
+from pathlib import Path
 
-path = 'tracedata/16-09-27.csv' #ファイルパスの設定
+# path = Path("2018-11-1815_time/") #ファイルパスの設定
+# filelist = list(path.glob("*"))
+filelist = glob.glob("2018-11-1815_time/*")
 
+for csvfilename in filelist:
+#     name = '2018-11-1815_time/'
+#     filename = name+csvfilename
+    print(csvfilename)
+    data = pd.read_csv(csvfilename)
+    left = data['No. ']
+    height = data['length']
+  
+    plt.plot(left,height)
+    plt.show()
 
+print(filelist)
 # ファイルの読み込み
-def callFile():
-    with open(path) as fp:
-        csv_list = list(csv.reader(fp))
+# def callFile():
+#     with open(path) as fp:
+#         csv_list = list(csv.reader(fp))
 
 #平均
 # def average():
@@ -17,4 +34,4 @@ def callFile():
  
 
 
-callFile()
+# callFile()
